19ad6440dc56af91686fa32d2540f844b8b9a234
General <Thomas.Urias49@yahoo.com>
2025-01-21 18:37:02 -0700

    Updated Salaheem spirit script formatting and remove loop

--- scripts/actions/mobskills/salaheem_spirit.lua
+++ scripts/actions/mobskills/salaheem_spirit.lua
@@ -15,12 +15,22 @@ mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local tick = 10
     local duration = 50
     local bonusTime = math.floor(mob:getTP() / 10) + 5
-    local effects = { xi.effect.STR_BOOST, xi.effect.DEX_BOOST, xi.effect.VIT_BOOST, xi.effect.AGI_BOOST, xi.effect.INT_BOOST, xi.effect.MND_BOOST, xi.effect.CHR_BOOST }
 
-        for i = 1, #effects do
-            target:delStatusEffect(effects[i])
-            target:addStatusEffect(effects[i], power, tick, duration + bonusTime)
-        end
+    target:delStatusEffect(xi.effect.STR_BOOST)
+    target:delStatusEffect(xi.effect.DEX_BOOST)
+    target:delStatusEffect(xi.effect.VIT_BOOST)
+    target:delStatusEffect(xi.effect.AGI_BOOST)
+    target:delStatusEffect(xi.effect.INT_BOOST)
+    target:delStatusEffect(xi.effect.MND_BOOST)
+    target:delStatusEffect(xi.effect.CHR_BOOST)
+
+    target:addStatusEffect(xi.effect.STR_BOOST, power, tick, duration + bonusTime)
+    target:addStatusEffect(xi.effect.DEX_BOOST, power, tick, duration + bonusTime)
+    target:addStatusEffect(xi.effect.VIT_BOOST, power, tick, duration + bonusTime)
+    target:addStatusEffect(xi.effect.AGI_BOOST, power, tick, duration + bonusTime)
+    target:addStatusEffect(xi.effect.INT_BOOST, power, tick, duration + bonusTime)
+    target:addStatusEffect(xi.effect.MND_BOOST, power, tick, duration + bonusTime)
+    target:addStatusEffect(xi.effect.CHR_BOOST, power, tick, duration + bonusTime)
 
     skill:setMsg(xi.msg.basic.STATUS_BOOST_2)
 
